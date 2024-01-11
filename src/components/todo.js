dad9e5f5290ae2986735780e67df35346b6a0355
KvJellyBean <kevinnatan91@gmail.com>
2024-01-11 18:36:46 +0700

    Create to do object

---
+++ src/components/todo.js
@@ -0,0 +1,8 @@
+// To do object
+const Todo = (title, description, due, priority, project, status) => {
+    return {
+        title, description, due, priority, project, status
+    }
+};
+
+export default Todo;
