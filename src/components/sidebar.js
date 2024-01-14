bde2f005765d3171588b66ee8fb388bafd2c2b22
KvJellyBean <kevinnatan91@gmail.com>
2024-01-14 21:35:59 +0700

    Change svg element to img

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,3 +1,12 @@
+import DOM from './dom.js';
+import EventHandler from './eventHandler.js';
+import allTasks from '../images/allTask.svg';
+import today from '../images/today.svg';
+import upcoming from '../images/upcoming.svg';
+import priority from '../images/priority.svg';
+import completed from '../images/completed.svg';
+import addProject from '../images/addProject.svg';
+
 const Sidebar = (() => {
     function initSidebar() {
         const sidebar = document.querySelector('nav');
@@ -6,6 +15,10 @@ const Sidebar = (() => {
         const project = createProjectContainer();
 
         sidebar.append(home, project);
+        DOM.showProject();
+        EventHandler.showHomeTodos();
+        EventHandler.addProjectButton();
+        EventHandler.showProjectTodos();
         return sidebar;
     }
 
@@ -16,16 +29,16 @@ const Sidebar = (() => {
         container.innerHTML = `
             <h2>Home</h2>
             <hr>    
-            <ul>
-                <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Zm280 240q-17 0-28.5-11.5T440-440q0-17 11.5-28.5T480-480q17 0 28.5 11.5T520-440q0 17-11.5 28.5T480-400Zm-160 0q-17 0-28.5-11.5T280-440q0-17 11.5-28.5T320-480q17 0 28.5 11.5T360-440q0 17-11.5 28.5T320-400Zm320 0q-17 0-28.5-11.5T600-440q0-17 11.5-28.5T640-480q17 0 28.5 11.5T680-440q0 17-11.5 28.5T640-400ZM480-240q-17 0-28.5-11.5T440-280q0-17 11.5-28.5T480-320q17 0 28.5 11.5T520-280q0 17-11.5 28.5T480-240Zm-160 0q-17 0-28.5-11.5T280-280q0-17 11.5-28.5T320-320q17 0 28.5 11.5T360-280q0 17-11.5 28.5T320-240Zm320 0q-17 0-28.5-11.5T600-280q0-17 11.5-28.5T640-320q17 0 28.5 11.5T680-280q0 17-11.5 28.5T640-240Z"/></svg> All Tasks</a></li>
+            <ul id="home-categories">
+                <li><a href="#" class="allTasks"><img class="allTasks" src="${allTasks}" alt="All Tasks" /> All Tasks</a></li>
     
-                <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M360-300q-42 0-71-29t-29-71q0-42 29-71t71-29q42 0 71 29t29 71q0 42-29 71t-71 29ZM200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Z"/></svg> Today</a></li>
+                <li><a href="#" class="today"><img class="today" src="${today}" alt="Today" />Today</a></li>
     
-                <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M600-80v-80h160v-400H200v160h-80v-320q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H600ZM320 0l-56-56 103-104H40v-80h327L264-344l56-56 200 200L320 0ZM200-640h560v-80H200v80Zm0 0v-80 80Z"/></svg> Upcoming</a></li>
+                <li><a href="#" class="upcoming"><img class="upcoming" src="${upcoming}" alt="Upcoming" /> Upcoming</a></li>
     
-                <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-120q-33 0-56.5-23.5T400-200q0-33 23.5-56.5T480-280q33 0 56.5 23.5T560-200q0 33-23.5 56.5T480-120Zm-80-240v-480h160v480H400Z"/></svg> Priority</a></li>
+                <li><a href="#" class="priority"><img class="priority" src="${priority}" alt="Priority" /> Priority</a></li>
     
-                <li><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M400-240v-80h240v80H400Zm-158 0L15-467l57-57 170 170 366-366 57 57-423 423Zm318-160v-80h240v80H560Zm160-160v-80h240v80H720Z"/></svg> Completed</a></li>
+                <li><a href="#" class="completed"><img class="completed" src="${completed}" alt="Completed" /> Completed</a></li>
             </ul>
             <hr>
         `;
@@ -39,7 +52,7 @@ const Sidebar = (() => {
         container.innerHTML = `
             <div class="project-container">
                 <h2>Project</h2>
-                <svg class="addProject" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M520-400h80v-120h120v-80H600v-120h-80v120H400v80h120v120ZM320-240q-33 0-56.5-23.5T240-320v-480q0-33 23.5-56.5T320-880h480q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H320Zm0-80h480v-480H320v480ZM160-80q-33 0-56.5-23.5T80-160v-560h80v560h560v80H160Zm160-720v480-480Z"/></svg>
+                <img class="addProject" src="${addProject}" alt="Add Project" />
             </div>
             <hr>
                 <ul id="project-list">
